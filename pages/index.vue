3ab0ea51a8425d1a04ef96ca088830c7daf55189
chen-L-miku <632237283@qq.com>
2019-07-30 21:00:05 +0800

    轮播图完成

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,46 @@
 <template>
-  <div>首页</div>
+  <div class="block">
+    <el-carousel height="750px">
+      <el-carousel-item v-for="item in img" :key="item"
+      class="mawari"
+      :style='`background:url(${item.url}) center center no-repeat;
+      background-size:contain contain;`'
+      >
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
 <script>
 export default {
- 
+  data (){
+    return{
+      img:[
+        {url:'_nuxt/pages/44b5801a9046ea1d598c3cb7102ecab1eb7fdecf.jpg@1320w_828h.jpg'},
+        {url:'_nuxt/pages/5c73b189205de.jpg'},{url:'_nuxt/pages/5c73b18c3e9eb.jpg'},{url:'_nuxt/pages/5c88ad75ab0df.jpg'}
+      ]
+    }
+  }
 }
 </script>
-
 <style>
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 150px;
+    margin: 0;
+  }
 
+  .el-carousel__item:nth-child(2n) {
+     background-color: #99a9bf;
+  }
+  
+  .el-carousel__item:nth-child(2n+1) {
+     background-color: #d3dce6;
+     
+  }
+  .mawari{
+    width: 100%;
+    height: 100%;
+  }
 </style>
--- pages/user/login.vue
+++ pages/user/login.vue
@@ -6,6 +6,6 @@ export default {
     
 }
 </script>
-<style lang="less" scoped>
+<style lang="less">
 
 </style>
